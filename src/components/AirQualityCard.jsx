bac928ab94df6239fe9c24e8e146d5d8f17e42f2
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2023-06-24 23:51:17 -0700

    Updated Content

--- src/components/AirQualityCard.jsx
+++ src/components/AirQualityCard.jsx
@@ -4,19 +4,19 @@ const getCardColor = (aqi) => {
     if (aqi <= 50) {
         // Good
         // return 'bg-success text-white';
-        return 'bg-green-500 text-white';
+        return 'bg-emerald-600 text-white';
     } else if (aqi <= 100) {
         // Moderate
         return 'bg-yellow-500';
     } else if (aqi <= 150) {
         //Unhealthy for Sensitive group
-        return 'bg-amber-500';
+        return 'bg-amber-600';
     } else if (aqi <= 200) {
         //Unhealthy
-        return 'bg-rose-800 text-white';
+        return 'bg-rose-700 text-white';
     } else if (aqi <= 300) {
         //Very Unhealthy
-        return 'bg-very-unhealthy text-white';
+        return 'bg-red-900 text-white';
     } else {
         return 'bg-hazardous ';
     }
@@ -25,19 +25,31 @@ const getCardColor = (aqi) => {
 const AirQualityCard = ({ data }) => {
     const { aqi, city, dominentpol, time } = data
     // const color = getCardColor(aqi)
+    //Color Test
     const color = getCardColor(200)
 
     return (
         <>
-            <div className={`p-5 border rounded-xl my-2 ${color}`}>
-                <div className={`card`}>
+            {/* Color Test */}
+            <div>
+                <button className="button">Good</button>
+                <button className="button">Moderate</button>
+                <button className="button">Unhealthy(s)</button>
+                <button className="button">Unhealthy</button>
+                <button className="button">Very UnHealthy</button>
+                <button className="button">Hazardous</button>
+            </div>
+            {/* <div className={`p-5 border rounded-xl my-2 `}> */}
+            <div className={`card shadow-xl ${color}`}>
+                <div className="card-body">
                     <h5 className="card-title">{city.name}</h5>
-                    <h6 className="font-semibold">Air Quality Index: {aqi}</h6>
+                    <h6 className="">Air Quality Index: {aqi}</h6>
                     <p>Dominant Pollutant: {dominentpol}</p>
                     <p>Last Updated: {time.s}</p>
                 </div>
             </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            {/* </div> */}
+            {/* <div className="card shadow-xl">
                 <div className="card-body">
                     <h2 className="card-title">Card title!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -45,7 +57,7 @@ const AirQualityCard = ({ data }) => {
                         <button className="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </>
     )
 }
