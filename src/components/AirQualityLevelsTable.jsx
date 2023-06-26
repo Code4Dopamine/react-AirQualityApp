77426a976649d5d76860921bbb36ce0cae149ebc
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2023-06-25 23:33:01 -0700

    Updated Content

---
+++ src/components/AirQualityLevelsTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AirQualityLevelsTable = () => {
+  return (
+    <div>AirQualityLevelsTable</div>
+  )
+}
+
+export default AirQualityLevelsTable
---
+++ src/components/PollutantInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PollutantInfo = () => {
+  return (
+    <div>PollutantInfo</div>
+  )
+}
+
+export default PollutantInfo
